c2130a870f247bbf6f2c666219dc1395fb790b75
John Macaluso <jmacaluso@usnymjmacaluso2.myfiosgateway.com>
2021-04-16 12:57:14 -0400

    Add angular function to toggle accordion

--- angular/src/app/accordion/accordion.component.ts
+++ angular/src/app/accordion/accordion.component.ts
@@ -12,4 +12,22 @@ export class AccordionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleAccordion(event) {
+    const element = event.currentTarget;
+    const panel = element.nextElementSibling;
+    const chev = element.querySelector('.chevron');
+    element.classList.toggle('is-expanded');
+    element.classList.toggle('is-selected');
+    chev.classList.toggle('active');
+
+    if (panel.style.height === 'auto') {
+      panel.style.height = 0;
+      panel.classList.remove('is-expanded');
+      console.log(panel);
+    } else {
+      panel.style.height = 'auto';
+      panel.classList.add('is-expanded');
+    }
+  }
+
 }
